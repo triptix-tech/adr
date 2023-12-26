ffb73fb1f2da5cbe96f195b7ab17b8ac40a242ea
Felix Gündling <felix.guendling@gmail.com>
2023-12-26 15:59:45 +0100

    wip

--- exe/server.cc
+++ exe/server.cc
@@ -73,54 +73,38 @@ int main(int ac, char** av) {
   workers.resize(std::thread::hardware_concurrency());
   for (auto& w : workers) {
     w = std::thread{[&]() {
-      return new std::thread([&]() {
-        auto in = std::string{};
-        auto ctx = adr::guess_context{cache};
-        auto lang_list =
-            std::basic_string<adr::language_idx_t>{adr::kDefaultLang};
-        ctx.resize(*t);
-        auto server =
-            uWS::SSLApp(uWS::SocketContextOptions{
-                            .key_file_name = "deps/uWebSockets/misc/key.pem",
-                            .cert_file_name = "deps/uWebSockets/misc/cert.pem",
-                            .passphrase = "1234",
-                            .ca_file_name = "cert.pem"})
-                .get("/g/:req",
-                     [&](uWS::HttpResponse<true>* res, uWS::HttpRequest* req) {
-                       adr::url_decode(req->getParameter(0), in);
-                       res->end(request(in, *t, ctx, adr::api::kGPlaces));
-                       res->writeHeader("Content-Type",
-                                        "application/json;charset=UTF-8");
-                     })
-                .get("/mb/:req",
-                     [&](uWS::HttpResponse<true>* res, uWS::HttpRequest* req) {
-                       adr::url_decode(req->getParameter(0), in);
-                       res->end(request(in, *t, ctx, adr::api::kMB));
-                       res->writeHeader("Content-Type",
-                                        "application/json;charset=UTF-8");
-                     })
-                .get("/pelias/:req",
-                     [&](uWS::HttpResponse<true>* res, uWS::HttpRequest* req) {
-                       adr::url_decode(req->getParameter(0), in);
-                       res->end(request(in, *t, ctx, adr::api::kPelias));
-                       res->writeHeader("Content-Type",
-                                        "application/json;charset=UTF-8");
-                     })
-                .listen(port, [&](auto* listen_socket) {
-                  if (listen_socket) {
-                    std::cout << "Thread " << std::this_thread::get_id()
-                              << " listening on port " << port << std::endl;
-                  } else {
-                    std::cout << "Thread " << std::this_thread::get_id()
-                              << " failed to listen on port port" << std::endl;
-                  }
-                });
-        auto const ssl_context = server.getNativeHandle();
-        SSL_CTX_set_verify((SSL_CTX*)ssl_context,
-                           SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT,
-                           NULL);
-        server.run();
-      });
+      auto in = std::string{};
+      auto ctx = adr::guess_context{cache};
+      auto lang_list =
+          std::basic_string<adr::language_idx_t>{adr::kDefaultLang};
+      ctx.resize(*t);
+      auto server =
+          uWS::SSLApp(uWS::SocketContextOptions{
+                          .key_file_name = "deps/uWebSockets/misc/key.pem",
+                          .cert_file_name = "deps/uWebSockets/misc/cert.pem",
+                          .passphrase = "1234",
+                          .ca_file_name = "cert.pem"})
+              .get("/g/:req",
+                   [&](uWS::HttpResponse<true>* res, uWS::HttpRequest* req) {
+                     adr::url_decode(req->getParameter(0), in);
+                     res->end(request(in, *t, ctx, adr::api::kGPlaces));
+                     res->writeHeader("Content-Type",
+                                      "application/json;charset=UTF-8");
+                   })
+              .listen(port, [&](auto* listen_socket) {
+                if (listen_socket) {
+                  std::cout << "Thread " << std::this_thread::get_id()
+                            << " listening on port " << port << std::endl;
+                } else {
+                  std::cout << "Thread " << std::this_thread::get_id()
+                            << " failed to listen on port port" << std::endl;
+                }
+              });
+      auto const ssl_context = server.getNativeHandle();
+      SSL_CTX_set_verify((SSL_CTX*)ssl_context,
+                         SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT,
+                         NULL);
+      server.run();
     }};
   }
 
@@ -128,6 +112,5 @@ int main(int ac, char** av) {
     w.join();
   }
 
-  std::cout << "Failed to listen on port " << port << std::endl;
   return 1;
 }
