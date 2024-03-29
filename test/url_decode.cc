46bb2c3f9b10be204afc97723cb7f2f7a354425f
Felix Gündling <felix.guendling@gmail.com>
2024-03-29 16:22:39 +0100

    cleanup

--- test/url_decode.cc
+++
@@ -1,9 +0,0 @@
-#include "gtest/gtest.h"
-
-#include "adr/url_decode.h"
-
-TEST(adr, url_decode) {
-  std::string out;
-  adr::url_decode("Test%20%23%C3%A4%C3%B6%C3%BC77%2610", out);
-  EXPECT_EQ("Test #äöü77&10", out);
-}
